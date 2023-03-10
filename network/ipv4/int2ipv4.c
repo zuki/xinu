50b4f6e389901de3da2a3e06853213898df68876
Keiji Suzuki <zuki.ebetsu@gmail.com>
2023-03-10 10:25:08 +0900

    ipv42int()を作成

--- network/ipv4/int2ipv4.c
+++ network/ipv4/int2ipv4.c
@@ -18,7 +18,7 @@
  * @return
  *      変換が成功したら ::OK; それ以外は ::SYSERR.
  */
-syscall int2ipv4(uint32_t addr, struct netaddr *ip)
+syscall int2ipv4(const uint32_t addr, struct netaddr *ip)
 {
     /* 1. 引数のエラーチェック */
     if (NULL == ip)
@@ -32,5 +32,29 @@ syscall int2ipv4(uint32_t addr, struct netaddr *ip)
     ip->addr[3] = (uint8_t) ((addr >> 0)  & 0xff);
     ip->type = NETADDR_IPv4;
     ip->len = IPv4_ADDR_LEN;
+    return OK;
+}
+
+/**
+ * @ingroup ipv4
+ *
+ * ネットアドレスをuint32_tに変換する.
+ *
+ * @param ip 変換するnetaddr構造体
+ * @param addr 変換結果を格納するuint32_tへのポインタ
+ *
+ * @return
+ *      変換が成功したら ::OK; それ以外は ::SYSERR.
+ */
+syscall ipv42int(const struct netaddr *ip, uint32_t *addr)
+{
+    /* 1. 引数のエラーチェック */
+    if (NULL == ip || NULL == addr)
+    {
+        return SYSERR;
+    }
+
+    *addr = (((uint32_t)ip->addr[0]) << 24) | (((uint32_t)ip->addr[1]) << 16) |
+            (((uint32_t)ip->addr[2]) << 8)  | (((uint32_t)ip->addr[3]) << 0);
     return OK;
 }
